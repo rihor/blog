42c6f7f630fdd94aad73260594cddda452b67618
Pedro Pinho <rihor1000@gmail.com>
2020-11-11 10:57:03 -0300

    Showing pixelRatio on bottom

--- src/components/R3F/EerieScene.tsx
+++ src/components/R3F/EerieScene.tsx
@@ -12,24 +12,29 @@ interface EerieSceneProps {
 
 const EerieScene: React.FC<EerieSceneProps> = ({ isMobile, mouse }) => {
   return (
-    <Canvas
-      pixelRatio={Math.min(2, isMobile ? window.devicePixelRatio : 1)}
-      camera={{ fov: 100, position: [0, 0, 30] }}
-      onCreated={({ gl }) => {
-        gl.toneMapping = ReinhardToneMapping
-        gl.setClearColor(new Color('#03030a'))
-      }}
-      gl={{
-        powerPreference: 'high-performance'
-      }}
-    >
-      <fog attach="fog" args={['#040412', 50, 150]} />
-      <pointLight distance={100} intensity={2} color="lightblue" />
-      <ambientLight intensity={10} color="lightblue" />
+    <>
+      <Canvas
+        pixelRatio={Math.min(2, isMobile ? window.devicePixelRatio : 1)}
+        camera={{ fov: 100, position: [0, 0, 30] }}
+        onCreated={({ gl }) => {
+          gl.toneMapping = ReinhardToneMapping
+          gl.setClearColor(new Color('#03030a'))
+        }}
+        gl={{
+          powerPreference: 'high-performance'
+        }}
+      >
+        <fog attach="fog" args={['#040412', 50, 150]} />
+        <pointLight distance={100} intensity={2} color="lightblue" />
+        <ambientLight intensity={10} color="lightblue" />
 
-      <Particles count={isMobile ? 5000 : 8000} mouse={mouse} />
-      <Effects />
-    </Canvas>
+        <Particles count={isMobile ? 5000 : 8000} mouse={mouse} />
+        <Effects />
+      </Canvas>
+      <h2 style={{ color: 'white', fontSize: 50 }}>
+        {String(Math.min(2, isMobile ? window.devicePixelRatio : 1))}
+      </h2>
+    </>
   )
 }
 
