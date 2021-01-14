49e1e700b46d3fcbaf7787a1808ecf0889f768be
Pedro Pinho <rihor1000@gmail.com>
2021-01-14 10:36:24 -0300

    Upgrading all possible dependencies and tsconfig strict true

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 // next.config.js
 const withTM = require("next-transpile-modules")([
-  // "@react-three/drei",
-  // "three",
-  "postprocessing",
-  // "@react-three/postprocessing"
+  "@react-three/drei",
+  "three",
+  // "postprocessing",
+  "@react-three/postprocessing"
 ]) // pass the modules you would like to see transpiled
 
 module.exports = withTM()
